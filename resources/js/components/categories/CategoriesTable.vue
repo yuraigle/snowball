4ef21d8ff4c73f134b800be6ae53075c834ccb99
yuraigle <yuraigle@gmail.com>
2023-02-15 08:16:17 +0800

    ...

--- resources/js/components/categories/CategoriesTable.vue
+++ resources/js/components/categories/CategoriesTable.vue
@@ -1,22 +1,45 @@
 <template>
-    <table class="border small" style="width: 500px">
+    <table class="border small" style="width: 550px">
         <template v-for="c0 in childrenFor(null)">
             <tr class="border-bottom">
-                <td class="c_name" style="padding-left: 10px">
-                    <img v-if="c0['icon']" :src="`/layout/${c0['icon']}`" alt="" width="32" class="me-2"/>
-                    <img v-if="!c0['ticker']" src="/layout/pie-chart.svg" alt="" width="32" class="me-2"/>
+                <td v-if="c0['ticker']" class="c_name" style="padding-left: 10px">
+                    <img :src="`/layout/${c0['icon']}`" alt="" width="32" class="me-3"/>
                     {{ c0['name'] }}
                 </td>
-                <td class="c_weight">{{ c0['target_weight'] }}</td>
+                <td v-else class="c_name" style="padding-left: 10px">
+                    <img src="/layout/pie-chart.svg" alt="" width="32" class="me-2"/>
+                    <input v-model="names[c0['id']]" type="text" class="form-control">
+                </td>
+                <td class="c_weight">
+                    <input type="text" v-model="weights[c0['id']]" class="form-control">
+                </td>
+                <td class="text-end" style="width: 100px">
+                    <button class="btn btn-link px-2" v-if="!c0['ticker']">
+                        <i class="fa-solid fa-plus"></i>
+                    </button>
+                    <button class="btn btn-link px-2" v-if="!c0['ticker']">
+                        <i class="fa-solid fa-folder-plus"></i>
+                    </button>
+                </td>
             </tr>
             <template v-for="c1 in childrenFor(c0['id'])">
                 <tr class="border-bottom">
-                    <td class="c_name" style="padding-left: 30px">
-                        <img v-if="c1['icon']" :src="`/layout/${c1['icon']}`" alt="" width="32" class="me-2"/>
-                        <img v-if="!c1['ticker']" src="/layout/pie-chart.svg" alt="" width="32" class="me-2"/>
+                    <td v-if="c1['ticker']" class="c_name" style="padding-left: 30px">
+                        <img :src="`/layout/${c1['icon']}`" alt="" width="32" class="me-3"/>
                         {{ c1['name'] }}
                     </td>
-                    <td class="c_weight">{{ c1['target_weight'] }}</td>
+                    <td v-else class="c_name" style="padding-left: 30px">
+                        <img src="/layout/pie-chart.svg" alt="" width="32" class="me-2"/>
+                        <input v-model="names[c1['id']]" type="text" class="form-control">
+                    </td>
+                    <td class="c_weight">
+                        <input type="text" v-model="weights[c1['id']]" class="form-control">
+                    </td>
+                    <td class="text-end" style="width: 100px">
+                        <button class="btn btn-link px-2">
+                            <i class="fa-solid fa-xmark"></i>
+                        </button>
+                    </td>
                 </tr>
             </template>
         </template>
@@ -28,7 +51,15 @@ export default {
     data() {
         return {
             stats: window.stats,
+            weights: {},
+            names: {},
         }
+    },
+    mounted() {
+        window.stats.forEach(c => {
+            this.weights[c['id']] = c['target_weight'];
+            this.names[c['id']] = c['name'];
+        });
     },
     methods: {
         childrenFor(parentId) {
@@ -42,12 +73,28 @@ export default {
 tr {
     height: 49px;
 }
-.c_name {
-    padding: 8px
+
+.c_weight > input {
+    width: 75px;
+    text-align: right;
+    background: transparent;
+    border: 1px solid #fff;
 }
 
-.c_weight {
-    padding: 8px;
-    text-align: right;
+.c_weight > input:hover, .c_weight > input:active, .c_weight > input:focus {
+    background: #edf1f5;
+    border: 1px solid #dee2e6;
+}
+
+.c_name > input {
+    width: 250px;
+    display: inline-block;
+    background: transparent;
+    border: 1px solid #fff;
+}
+
+.c_name > input:hover, .c_name > input:active, .c_name > input:focus {
+    background: #edf1f5;
+    border: 1px solid #dee2e6;
 }
 </style>
